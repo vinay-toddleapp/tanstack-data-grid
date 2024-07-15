0d06c7d2b4c3499fe7ff5bcdd56e8756d33c3f17
vinay-toddleapp <vinay.pratapsingh@toddleapp.com>
2024-07-15 08:35:56 +0530

    feat: editable table example added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import BasicTable from "./components/BasicTable";
+import EditableTable from "./components/EditableTable/EditableTable";
 
 const App = () => {
   return (
     <div>
-      <BasicTable />
+      <EditableTable />
     </div>
   );
 };
